3576d23adac3696d51741fcadbd2cf216b1a67e5
Luis Lennin Villarreal Castro <syslenninkadasoftware@gmail.com>
2023-02-01 16:09:38 -0600

    Creacion de models Arte,Opinion,Usuario.

---
+++ DigitalArt/Models/Opinione.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalArt.Models;
+
+public partial class Opinione
+{
+    public int IdOpinion { get; set; }
+
+    public string? NombreOpinion { get; set; }
+
+    public string? DescripcionOpinion { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+}
